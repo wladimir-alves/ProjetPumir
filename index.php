d7bb65f3f20d39957d994889ee162b1c85a7ad48
wladimir alves <wladimiralves85@hotmail.com>
2015-02-13 14:13:08 +0100

    new files

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Projet Pumir</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="font-icons/css/font-awesome.min.css">
+
+    </head>
+    <body>
+        <header>
+            <div id="connexion" class="connection">
+                <p><a href="">Connexion</a></p>
+            </div>
+            <div id="inscription" class="connection">
+                <p><a href="">Inscription</a></p>
+            </div>
+                
+                
+        </header>
+
+        <div id="landing-page">
+            <h1 id="title-lp">This is Panel One</h1>
+            
+            <p id="title-bigphoto">
+                Lorem ipsum
+            </p>
+            
+            <i class="fa fa-chevron-down fa-1x"></i>
+        </div>
+        <div id="main content" class="">
+
+        <h2>Sqlut</h2>
+
+            <aside id="main-menu" class="home-page">
+                <ul>
+                    <li><a href="">Accueil</a></li>
+                    <li><a href=""> Categories</a></li>
+                </ul>
+                
+                
+
+                
+                        
+            </aside>
+            <div id="content" class="">
+
+                <div id="">
+                    <div></div>
+                    
+                </div>
+
+                
+
+            </div>
+
+        </div>
+
+        
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
